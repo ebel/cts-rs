45924d5fe88266b499de412a11860ed61577fca3
bruhboy944 <tebel@tylerebel.com>
2024-04-28 21:30:14 -0700

    adding clap and another method for testing

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,128 @@
 use aws_config::meta::region::RegionProviderChain;
 use aws_config::BehaviorVersion;
-use aws_sdk_controltower as controltower;
-use colored::*; // Import the colored crate
+//use aws_sdk_controltower as controltower;
+use aws_sdk_controltower::operation::list_baselines::ListBaselinesOutput; // Correct import for ListBaselinesOutput
+use aws_sdk_controltower::{Client as ControlTowerClient, Error as ControlTowerError};
 
+use colored::*; // Import the colored crate
+use clap::{Command};
 
 //Make calls to sdk DRY
 
+
+pub struct AwsControlTowerClient {
+    client: ControlTowerClient,
+}
+
+impl AwsControlTowerClient {
+    // Constructs a new `AwsControlTowerClient`.
+    pub async fn new() -> Result<Self, aws_sdk_controltower::Error> {
+        let region_provider = RegionProviderChain::default_provider().or_else("us-west-2");
+        let config = aws_config::defaults(BehaviorVersion::latest())
+            .region(region_provider)
+            .load()
+            .await;
+
+        let client = ControlTowerClient::new(&config);
+        Ok(AwsControlTowerClient { client })
+    }
+
+    // Method to list landing zones
+    pub async fn list_landing_zones(&self) -> Result<(), aws_sdk_controltower::Error> {
+        let response = self.client.list_landing_zones().send().await?;
+
+        if response.landing_zones.is_empty() {
+            println!("{}", "No landing zones returned".red());
+        } else {
+            for landing_zone in response.landing_zones {
+                if let Some(arn) = &landing_zone.arn {
+                    println!("{} {}", "Control Tower Landing Zone ARN is:".blue(), arn);
+                } else {
+                    println!("{}", "No ARN available for this landing zone".yellow());
+                }
+            }
+        }
+        Ok(())
+    }
+
+    // Method to list enabled controls for a given target identifier
+    pub async fn list_enabled_controls(&self, target_identifier: &str) -> Result<(), aws_sdk_controltower::Error> {
+        let response = self.client.list_enabled_controls().target_identifier(target_identifier).send().await?;
+
+        println!("{}", "Control Tower Control is:".blue());
+
+        if response.enabled_controls.is_empty() {
+            println!("No controls returned");
+        } else {
+            for control in &response.enabled_controls {
+                println!("{} {}", "Control Identifier:".blue().to_string(), control.control_identifier.as_ref().unwrap_or(&"None".to_string()));
+                println!("{} {}", "ARN:".blue().to_string(), control.arn.as_ref().unwrap_or(&"None".to_string()));
+                println!("{} {}", "Target Identifier:".blue().to_string(), control.target_identifier.as_ref().unwrap_or(&"None".to_string()));
+
+                let status = control.status_summary.as_ref()
+                    .and_then(|summary| summary.status.as_ref())
+                    .map(|status| status.to_string())
+                    .unwrap_or("None".to_string());
+                println!("  Status: {}", status);
+
+                let drift_status = control.drift_status_summary.as_ref()
+                    .and_then(|summary| summary.drift_status.as_ref())
+                    .map(|status| status.to_string())
+                    .unwrap_or("None".to_string());
+                println!("  Drift Status: {}", drift_status);
+
+                println!(); // Add a blank line for better separation between entries
+            }
+        }
+        Ok(())
+    }
+
+    // Lists the baselines in AWS Control Tower
+    pub async fn list_baselines(&self) -> Result<ListBaselinesOutput, ControlTowerError> {
+        match self.client.list_baselines().send().await {
+            Ok(response) => {
+                println!("Baselines:");
+                for baseline in &response.baselines {
+                    let arn = &baseline.arn;
+                    let name = &baseline.name;
+                    let _description = &baseline.description;
+                    println!("ARN: {}", arn);
+                    println!("Name: {}", name);
+                    let _description = baseline.description.as_deref().unwrap_or("No description available");                    println!(); // Adds a blank line for better separation
+                }
+
+                if let Some(next_token) = &response.next_token {
+                    println!("Next token for pagination: {}", next_token);
+                } else {
+                    println!("No more pages.");
+                }
+
+                Ok(response)
+            },
+            Err(e) => {
+                println!("Error fetching baselines: {}", e);
+                Err(ControlTowerError::from(e))
+            }
+        }
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<(), controltower::Error> {
-    // Setup AWS region
-    let region_provider = RegionProviderChain::default_provider().or_else("us-west-2");
+async fn main() -> Result<(), aws_sdk_controltower::Error> {
+    let app = Command::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION")); // No `takes_value()` needed
 
-    // Setup AWS configuration
-    let config = aws_config::defaults(BehaviorVersion::latest())
-        .region(region_provider)
-        .load()
-        .await;
+    let _matches = app.get_matches();
 
-    // Create a Control Tower client
-    let client = aws_sdk_controltower::Client::new(&config);
+    let control_tower_client = AwsControlTowerClient::new().await?;
+    control_tower_client.list_landing_zones().await?;
 
-    // Retrieve and display the current status of AWS Control Tower
-    let response = client.list_landing_zones().send().await?;
-
-
-    // Print out the status
-    if response.landing_zones.is_empty() {
-        println!("{}", "No landing zones returned".red());
-    } else {
-        for landing_zone in response.landing_zones {
-            if let Some(arn) = &landing_zone.arn {
-                println!("{} {}", "Control Tower Landing Zone ARN is:".blue(), arn);
-            } else {
-                println!("{}", "No ARN available for this landing zone".yellow());
-            }
-        }
-    }
-    // Lookup org name
     let target_identifier = "";
-    let response = client.list_enabled_controls().target_identifier(target_identifier).send().await?;
+    control_tower_client.list_enabled_controls(target_identifier).await?;
 
-    // Directly work with the response since it's not a Result type
-    if response.enabled_controls.is_empty() {
-        println!("No controls returned");
-    } else {
-
-        use colored::*; // Import the colored crate at the top of your Rust file
-
-        for control in &response.enabled_controls {
-            println!("{}", "Control Tower Control is:".blue());
-
-            // Use to_string directly on the optional references
-            let control_identifier_label = "Control Identifier:".blue().to_string();
-            let control_identifier = control.control_identifier.as_ref().map(|s| s.to_string()).unwrap_or("None".to_string());
-            println!("{} {}", control_identifier_label, control_identifier);
-
-            let arn_label = "ARN:".blue().to_string();
-            let arn = control.arn.as_ref().map(|s| s.to_string()).unwrap_or("None".to_string());
-            println!("{} {}", arn_label, arn);
-
-            let target_identifier_label = "Target Identifier:".blue().to_string();
-            let target_identifier = control.target_identifier.as_ref().map(|s| s.to_string()).unwrap_or("None".to_string());
-            println!("{} {}", target_identifier_label, target_identifier);
-
-            match &control.status_summary {
-                Some(status_summary) => {
-                    let status = status_summary.status.as_ref().map(|s| s.to_string()).unwrap_or("None".to_string());
-                    println!("  Status: {}", status);
-                },
-                None => println!("  Status: None"),
-            }
-
-            match &control.drift_status_summary {
-                Some(drift_status_summary) => {
-                    let drift_status = drift_status_summary.drift_status.as_ref().map(|s| s.to_string()).unwrap_or("None".to_string());
-                    println!("  Drift Status: {}", drift_status);
-                },
-                None => println!("  Drift Status: None"),
-            }
-
-            println!(); // Add a blank line for better separation between entries
-        }
-
-
-    }
+    control_tower_client.list_baselines().await?;
 
     Ok(())
 }
